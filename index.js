9be920a1aaa3ddf2f21a61bca581a96e22c0454a
Martha Solomon <marthsolomon@gmail.com>
2022-01-21 17:52:41 -0500

    why are my results negative????

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    let counter = 42
+    let distance = value - counter
+    let blocks = distance 
+    return blocks
+}
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(value) {
+    let counter = 42
+    let distance = value - counter
+    let feet = distance * 264
+    return feet
+}
+
+distanceFromHqInFeet (38);
+
+function distanceTravelledInFeet(value, value2) {
+   let distance = value2 - value
+   let feet = distance * 264
+   return feet
+}
+
+distanceTravelledInFeet(48, 43);
+
+function calculatesFarePrice(start, destination) {
+    let feet = (start - destination) * 264
+    
+    if (feet < 400) {
+        return "gives customers a free sample"
+    } else if (feet > 400 ) {
+        let price = ((feet - 400) * 0.02 )
+        return price
+    } else { (feet >= 2500) 
+        return "cannot travel that far"
+    }
+
+}
+
+calculatesFarePrice(48, 43);
